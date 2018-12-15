e1f0e4cf6f8f4c3726a8bdac17f0ceb277e6bf41
SatbyulPark <hoi4821980@gmail.com>
2018-12-15 12:45:28 -0500

    dfdfd

---
+++ CoffeeAssignment/Models/Coffee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeAssignment.Models
+{
+    public class Coffee
+    {
+        
+            public int CoffeeId { get; set; }
+        [Required]
+        [Display(Name = "Coffee Name")]
+        public string CoffeeName { get; set; }
+        [Required]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+      
+        public decimal Description { get; set; }
+
+
+    }
+}
